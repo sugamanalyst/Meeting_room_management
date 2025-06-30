ad2b49bf891efa7e5037c074de62918c7b2faaf4
sugamanalyst <datanalyst_ops@sugamgroup.com>
2025-06-30 14:44:36 +0530

    Update meeting_room.py

--- meeting_room.py
+++ meeting_room.py
@@ -584,8 +584,8 @@ def view_reservations():
                 st.dataframe(past_df, hide_index=True)
 
 # --- Main App ---
-st.title("SUGAM GROUP ")
-st.title("_Meeting_Room_ :White[Booking System] :sunglasses:")
+st.title(" SUGAM GROUP ")
+st.title("_Meeting Room_ :white[Booking System] :office:")
 
 date = CURRENT_TIME_IST.date()
 time1 = CURRENT_TIME_IST.time()
